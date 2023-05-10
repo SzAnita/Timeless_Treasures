7a7d14c210a107552aaab542e1f862fc9d3ff33e
SzAnita <97391261+SzAnita@users.noreply.github.com>
2023-05-10 17:15:47 +0300

    update signup

--- antiques/forms.py
+++ antiques/forms.py
@@ -2,12 +2,14 @@ from django import forms
 from django.core import validators
 import re
 
+
 class Login(forms.Form):
     email = forms.EmailField(required=True, label='Email', validators=[validators.EmailValidator])
     pwd = forms.CharField(widget=forms.PasswordInput, required=True, label='Password', min_length=8)
 
+
 class Signup(forms.Form):
-    email = forms.EmailField(required=True, label='Email')
-    pwd = forms.CharField(widget=forms.PasswordInput, required=True, label='Password', min_length=8)
-    fname = forms.CharField(required=False, label='First Name(optional)')
-    lname = forms.CharField(required=False, label='Last Name(optional)')
+    email = forms.EmailField(required=True, widget=forms.TextInput(attrs={"placeholder": "Email"}))
+    pwd = forms.CharField(widget=forms.PasswordInput(attrs={"placeholder": "Password"}), required=True, label='Password', min_length=8)
+    fname = forms.CharField(required=False, widget=forms.TextInput(attrs={"placeholder": "First Name(optional"}))
+    lname = forms.CharField(required=False, widget=forms.TextInput(attrs={"placeholder": "Last Name(optional"}))
